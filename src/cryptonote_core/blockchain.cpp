b9544e6897bf0f184cb31d18e938e34559bb81ad
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2018-11-05 14:43:58 +0000

    blockchain: fix innocuous difficulty cache inconsistency
    This inconsistent state would not actually be used in practice
    
    ----
    From: Merge pull request #4806
    Commit Hash: ac23b10f152b483116419418c4cb11849285cf92
    
    Conflicts:
        src/cryptonote_core/blockchain.cpp
    Resolve:
        Merge both HEAD and incoming changes

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -826,6 +826,7 @@ difficulty_type Blockchain::get_difficulty_for_next_block()
   std::vector<difficulty_type> difficulties;
   auto height = m_db->height();
   size_t difficulty_blocks_count = get_current_hard_fork_version() < 10 ? DIFFICULTY_BLOCKS_COUNT : DIFFICULTY_BLOCKS_COUNT_V2;
+  top_hash = get_tail_id(); // get it again now that we have the lock
   // ND: Speedup
   // 1. Keep a list of the last 735 (or less) blocks that is used to compute difficulty,
   //    then when the next block difficulty is queried, push the latest height data and
